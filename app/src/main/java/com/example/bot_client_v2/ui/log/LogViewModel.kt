6350cd3f8ba8ec96168838d33aa8b117fb4eb27b
XieJiongyan <xjy-master@sjtu.edu.cn>
2021-11-20 16:35:19 +0800

    J.0.0.9 navigation

---
+++ app/src/main/java/com/example/bot_client_v2/ui/log/LogViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.bot_client_v2.ui.log
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LogViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
